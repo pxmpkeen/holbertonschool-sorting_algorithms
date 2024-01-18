b95ae66290d18a02be90d724bcfb1e2c7bf12d1a
MrNeonn75 <devneonn75@gmail.com>
2024-01-19 01:16:47 +0400

    2-selection_sort.c fix betty coding style

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -21,29 +21,29 @@ void swap(int *a, int *b)
  * sort algotrithm.
  * @array: Array to be sorted.
  * @size: Size of array given.
- * 
+ *
  * Return: Nothing.
  */
 void selection_sort(int *array, size_t size)
 {
-    unsigned int i, j, min_idx;
+	unsigned int i, j, min_idx;
 
-    if (!array)
+	if (!array)
         return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        }
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
 
-        if (i != min_idx)
-        {
-            swap(&array[min_idx], &array[i]);
-            print_array(array, size);
-        }
-    }
+		if (i != min_idx)
+		{
+			swap(&array[min_idx], &array[i]);
+			print_array(array, size);
+		}
+	}
 }
